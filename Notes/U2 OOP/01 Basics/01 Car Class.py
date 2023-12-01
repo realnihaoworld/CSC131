7446e382b030aae970d5e44045450c586b238371
Danison Zhang <89476288+realnihaoworld@users.noreply.github.com>
2023-12-01 10:45:04 -0600

    12/1 notes

---
+++ Notes/U2 OOP/01 Basics/01 Car Class.py
@@ -0,0 +1,45 @@
+
+
+class Car:
+    
+    # class variables go here
+    max_speed = 500
+    
+    def __init__(self, make, model):
+        """
+        This is the constructor
+        Gets called automatically on instantiation
+        We define instance variables here
+        """
+        self.make = make
+        self.model = model
+        self.x_position = 0
+        self.y_position = 0
+        
+    def forward(self):
+        self.x_position += 1
+    
+    def backward(self):
+        self.x_position -= 1
+
+    def __str__(self):
+        result = f"{self.make} {self.model}\n"
+        result += f"x: {self.x_position} y: {self.y_position}"
+        return result
+
+# MAIN
+
+my_car = Car("Honda", "Civic")
+your_car = Car("BMW", "M3")
+
+my_car.forward()
+my_car.forward()
+
+your_car.backward()
+
+print(my_car)
+print(your_car)
+
+print(my_car.x_position)
+print(your_car.x_position)
+
