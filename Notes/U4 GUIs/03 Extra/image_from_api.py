150948c8896fdbf2af7d26188b28e13bcaf8d53f
Danison Zhang <89476288+realnihaoworld@users.noreply.github.com>
2024-01-26 17:34:17 -0600

    pokemon api

--- Notes/U4 GUIs/03 Extra/image_from_api.py
+++ Notes/U4 GUIs/03 Extra/image_from_api.py
@@ -3,16 +3,20 @@ from tkinter import *
 import PIL.Image
 import PIL.ImageTk
 import io
+from random import randint
 
 window = Tk()
 
-# get a random url of a picture of a dog https://dog.ceo/api/breeds/image/random
-response = requests.get(url="https://dog.ceo/api/breeds/image/random")
+# get a random url of a picture of a dog https://dog.ceo/api/breeds/image/random | https://pokeapi.co/api/v2/pokemon
+# https://dog.ceo/api/breeds/image/random
+rand = randint(0, 1000)
+response = requests.get(url=f"https://pokeapi.co/api/v2/pokemon/{rand}/")
 data = response.json()
-print(data)
+print(rand)
+#print(data)
 
 # get image from random url
-response2 = requests.get(url=data["message"], stream=True)
+response2 = requests.get(url=data["sprites"]["front_default"], stream=True)
 
 # turn the image into a pillow image, and attach to Tkinter Label widget
 pillow_image = PIL.Image.open(io.BytesIO(response2.content))
